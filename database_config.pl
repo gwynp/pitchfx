8b5fefcf8853a554baec6b7ecd95c72aceb2483e
Gwyn Price <gwyn@gwynprice.com>
2013-11-07 17:05:02 -0500

    removed config file

--- database_config.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-# $dbh = DBI->connect("DBI:mysql:database=pitchfx;host=localhost", 'root', 'passPASS!!')
-
-my $database = 'database=pitchfx;host=localhost';
-my $dbuser = 'root';
-my $dbpass = 'bruc1e';    
